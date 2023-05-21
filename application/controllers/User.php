d0ab5755bab83c0d8a59e1d80114b02298121039
Faiz-Muazzam <Faizmuazzam.fm7@gmail.com>
2023-05-21 18:12:24 +0700

    [User] Update Delete Done

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -1,31 +1,34 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
-class User extends CI_Controller {
-    public function __construct(){
+defined('BASEPATH') or exit('No direct script access allowed');
+class User extends CI_Controller
+{
+    public function __construct()
+    {
         parent::__construct();
         $this->load->model("model_user");
         $this->load->library('form_validation');
     }
-    public function index(){
-        $data['tbl_user']=$this->model_user->tampil_data();
+    public function index()
+    {
+        $data['tbl_user'] = $this->model_user->tampil_data();
         $this->load->view('template/head');
         $this->load->view('template/sidebar');
         $this->load->view('template/topbar');
-        $this->load->view('user',$data);
-        $this->load->view('template/footer');       
-        
+        $this->load->view('user', $data);
+        $this->load->view('template/footer');
     }
 
-    function tambah(){
+    function tambah()
+    {
         $this->load->view('template/head');
         $this->load->view('template/sidebar');
         $this->load->view('template/topbar');
         $this->load->view('user/u_input');
         $this->load->view('template/footer');
-        
     }
-    
-    function tambah_aksi(){
+
+    function tambah_aksi()
+    {
         $nama = $this->input->post('nama');
         $username = $this->input->post('username');
         $password = $this->input->post('password');
@@ -42,17 +45,25 @@ class User extends CI_Controller {
     }
 
 
-    public function edit($nama){
-        $where = array('nama'=>$nama);
-        $data['user']= $this->model_user->edit_data($where,'tbl_user')->result();
+    public function edit($id)
+    {
+        // $where = array('id'=>$id);
+        // $data['user']= $this->model_user->edit_data($where,'tbl_user')->result();
+
+        $get_user = $this->model_user->get_by_id($id);
+        $data = array(
+            'user' => $get_user
+        );
+
         $this->load->view('template/head');
         $this->load->view('template/sidebar');
         $this->load->view('template/topbar');
-        $this->load->view('user/u_edit',$data);
+        $this->load->view('user/u_edit', $data);
         $this->load->view('template/footer');
     }
-    public function update(){
-        // $id = $this->input->post('id');
+    public function update()
+    {
+        $id = $this->input->post('id');
         $nama = $this->input->post('nama');
         $username = $this->input->post('username');
         $password = $this->input->post('password');
@@ -64,16 +75,27 @@ class User extends CI_Controller {
             'password' => $password,
             'kode_akses' => $kode_akses
         );
-        $where = array('nama' => $nama);
-        $this->model_user->update_data($where, $data, 'tbl_user');
+        // $where = array('nama' => $nama);
+        // $this->model_user->update_data($where, $data, 'tbl_user');
+        $this->model_user->update($id, $data);
         redirect('User/index');
     }
 
-    public function delete(){
-        $where = array('nama'=>$nama);
-        $data['user']= $this->model_user->hapus_data($where,'tbl_user');
-        redirect('User/index');
+    public function delete($id)
+    {
+        // $where = array('nama'=>$nama);
+        // $data['user']= $this->model_user->hapus_data($where,'tbl_user');
+        $row = $this->model_user->get_by_id($id);
+
+        if ($row) {
+            $this->model_user->delete($id);
+            $this->session->set_flashdata('message', 'Delete Record Success');
+            redirect('User/index');
+        } else {
+            $this->session->set_flashdata('message', 'Record Not Found');
+            redirect('User/index');
+        }
+        
     }
-    
+
 }
-?>
--- application/models/model_user.php
+++ application/models/model_user.php
@@ -1,30 +1,60 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
-class model_user extends CI_Model {
-    
+defined('BASEPATH') or exit('No direct script access allowed');
+class model_user extends CI_Model
+{
+
+    public $table = 'tbl_user';
+    public $id = 'id';
+
     // public function getUser(){
     //     $this->db->order_by('id','desc');
     //     return $this->db->get('tbl_user');
     // }
-    function tampil_data(){
+    function tampil_data()
+    {
         return $this->db->get('tbl_user');
     }
 
-    function input_data($data,$table){
-        $this->db->insert($table,$data);
+    function input_data($data, $table)
+    {
+        $this->db->insert($table, $data);
     }
 
-    function edit_data($where,$table){
-        return $this->db->get_where($table,$where);
+    function edit_data($where, $table)
+    {
+        return $this->db->get_where($table, $where);
     }
-    function update_data($where,$data,$table){
+    function update_data($where, $data, $table)
+    {
         $this->db->where($where);
-        $this->db->update($table,$data);
+        $this->db->update($table, $data);
     }
 
-    function hapus_data($where,$table){
+    function hapus_data($where, $table)
+    {
         $this->db->where($where);
         $this->db->delete($table);
-      }
+    }
+
+    // New Custom Edit/Update
+
+    // update data
+    function update($id, $data)
+    {
+        $this->db->where($this->id, $id);
+        $this->db->update($this->table, $data);
+    }
+
+    // delete data
+    function delete($id)
+    {
+        $this->db->where($this->id, $id);
+        $this->db->delete($this->table);
+    }
+
+    function get_by_id($id)
+    {
+        $this->db->where($this->id, $id);
+        return $this->db->get($this->table)->row();
+    }
 }
-?>
